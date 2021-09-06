f2ca0536053031567d5c8b605df19cc477c889e2
Francis Paolo <francispaolo.talan@gmail.com>
2021-09-07 03:41:11 +0800

    Added Functionality to check the board for Winning Conditions and shows the winner of the game

--- fpvt-projects/tic-tac-toe/javascript/script.js
+++ fpvt-projects/tic-tac-toe/javascript/script.js
@@ -20,27 +20,28 @@ const winState = [
 ]
 
 //BOARD GAME STATE
-let gameState = [
-    ['','',''],
-    ['','',''],
-    ['','','']
-];
-
+let gameState = [] //Empty array where we will push game history that makes it a 2D array
 
 //FUNCTIONS
 const selectCell = (e) => { 
     // console.log(e.target) //Check event target data
     const cellData = e.target;
+    let playerMark;
     // console.log(cellIndex); //Check index of selected or clicked target
     player = !player;
     if(player === true) {
         cellData.innerHTML = 'O';
+        playerMark = 'O';
     } else {
         cellData.innerHTML = 'X';
+        playerMark = 'X';
     }
+
+    classifyWinnner(playerMark);
     StoreMove(cellData);
 }
 
+//Functions that Stores the Moive in an Array the pushes the move in gameState
 const StoreMove = () => {
     let row1 = []
     let row2 = []
@@ -59,9 +60,29 @@ const StoreMove = () => {
         }
     })
 
-    console.log(row1);
-    console.log(row2);
-    console.log(row3);
+    gameState.push([row1, row2, row3]);
+    // console.log(gameState); //Checking if gameState is storing
+}
+
+
+//Checks the GameState for possible winning combinations
+const classifyWinnner = (playerMark) => {
+    for (let i = 0; i < boardCells.length; i++) {
+        // i = 0
+        for (let j = 0; j < winState.length; j++) { // loop the winner combi array
+            // j = 2
+            for (let k = 0; k < winState[j].length; k++) {
+                const tile1 = boardCells[winState[j][0]].innerText 
+                const tile2 = boardCells[winState[j][1]].innerText 
+                const tile3 = boardCells[winState[j][2]].innerText
+
+                if (tile1 === playerMark && tile2 === playerMark && tile3 === playerMark) {
+                    return console.log(`player ${playerMark} wins.)
+                }
+
+            }
+        }
+    }          
 }
 
 //CLICK EVENTS
