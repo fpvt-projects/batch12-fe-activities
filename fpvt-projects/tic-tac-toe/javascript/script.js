87e9f784c12426530ed3c7afb432053010ea95e9
Francis Paolo <francispaolo.talan@gmail.com>
2021-09-07 13:36:51 +0800

    Added Functionaliy for Previous, Reset, and Next Button

--- fpvt-projects/tic-tac-toe/javascript/script.js
+++ fpvt-projects/tic-tac-toe/javascript/script.js
@@ -5,7 +5,13 @@ console.log('Execute Program'); //Check if Script is working
 const modalDiv = document.querySelector('#modal-bg');
 const modalExit = document.querySelector('#modal-exit-button');
 const boardCells = document.querySelectorAll('.cell');
+const prevBtn = document.querySelector('.Previous');
+const nextBtn = document.querySelector('.Next')
+const resetBtn = document. querySelector('.Reset')
+const boardGame = document.querySelector('#actual-board')
 let player = true;
+let gameHistory = 0;
+let drawCounter = 0;
 
 //WINNING CONDITIONS AND COMBINATIONS
 const winState = [
@@ -60,34 +66,123 @@ const StoreMove = () => {
         }
     })
 
+    
     gameState.push([row1, row2, row3]);
+    gameHistory = gameState.length-1;
     // console.log(gameState); //Checking if gameState is storing
 }
 
 
 //Checks the GameState for possible winning combinations
 const classifyWinnner = (playerMark) => {
+
     for (let i = 0; i < boardCells.length; i++) {
-        // i = 0
-        for (let j = 0; j < winState.length; j++) { // loop the winner combi array
-            // j = 2
+        for (let j = 0; j < winState.length; j++) { 
             for (let k = 0; k < winState[j].length; k++) {
                 const tile1 = boardCells[winState[j][0]].innerText 
                 const tile2 = boardCells[winState[j][1]].innerText 
                 const tile3 = boardCells[winState[j][2]].innerText
 
                 if (tile1 === playerMark && tile2 === playerMark && tile3 === playerMark) {
-                    return console.log(`player ${playerMark} wins.)
-                }
-
+                    return console.log(`Player ${playerMark} Wins!`)
+                } 
             }
         }
-    }          
+    }
+
+    boardCells.forEach(cell => {
+        if(cell.innerText != '') {
+            drawCounter++
+        }
+    })
+
+    console.log(drawCounter === 9);
+
+    if(drawCounter === 9) {
+        console.log(`Draw!`);
+        //document
+    }
+
+    drawCounter = 0;
+    // console.log(drawCounter);
+
+
 }
+
+function prevMove () {
+
+    
+    // gameState[gameHistory];
+    // console.log(gameHistory)
+    // console.log(gameState[gameHistory])
+
+    if(gameHistory > 0){
+        gameHistory -= 1;
+        loadTiles(gameHistory);
+        
+    }
+    console.log(gameState);
+    console.log(gameHistory);
+
+    
+}
+
+function nextMove() {
+    
+
+    if (gameHistory >= 0 && gameHistory < gameState.length-1){
+        gameHistory += 1;
+        loadTiles(gameHistory);
+    }
+}
+
+function loadTiles (index) {
+    boardGame.innerHTML = '';
+
+    for (let i = 0; i < gameState[index].length; i++) {
+        for (let j = 0; j < gameState[index][i].length; j++) {
+            let div = document.createElement('div')
+
+            div.innerText = gameState[index][i][j]
+            div.classList.add('cell');
+            // div.addEventListener('click', selectCell);
+            console.log(div)
+            boardGame.append(div)
+        }
+    }
+
+    console.log(boardCells);
+}
+
+function resetBoard() {
+    player = true;
+    gameHistory = 0;
+    drawCounter = 0;
+
+
+    gameState = []
+    boardGame.innerHTML = '';
+
+    for(let i = 0; i < 9; i++) {
+        let div = document.createElement('div')
+
+             
+
+            div.innerText = ''
+            div.classList.add('cell');
+            div.addEventListener('click', selectCell);
+            console.log(div)
+            boardGame.append(div)
+    }
+}
+
+
 
 //CLICK EVENTS
 boardCells.forEach(boardCell => boardCell.addEventListener('click', selectCell));
-
+prevBtn.addEventListener('click', prevMove);
+nextBtn.addEventListener('click', nextMove);
+resetBtn.addEventListener('click', resetBoard);
 
 
 
@@ -152,13 +247,13 @@ boardCells.forEach(boardCell => boardCell.addEventListener('click', selectCell))
 //     console.log(cellIndex);
 //     cellInfo.innerHTML = 'O';
 
-//     //Prevent if game not running
-//     // for ( const i = 0; i < gameState.length; i++) {
-//     //     const gameState = gameState[i]
-//     //     for ( const j = 0; j <gameState.length; j++){
-//     //         console.log(i + j);
-//     //     }
-//     // }
+//     Prevent if game not running
+//     for ( const i = 0; i < gameState.length; i++) {
+//         const gameState = gameState[i]
+//         for ( const j = 0; j <gameState.length; j++){
+//             console.log(i + j);
+//         }
+//     }
 //     checkWin();
 // }
 
