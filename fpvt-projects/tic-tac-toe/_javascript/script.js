dcf5ac1c449886ef30560902a6e83f008e43ab92
Francis Paolo <francispaolo.talan@gmail.com>
2021-09-08 13:07:47 +0800

    Organized code files for a clean path/directory

--- fpvt-projects/tic-tac-toe/_javascript/script.js
+++ fpvt-projects/tic-tac-toe/_javascript/script.js
@@ -1,17 +1,37 @@
 'use strict'
 console.log('Execute Program'); //Check if Script is working
 
-//VARIABLES
-const modalDiv = document.querySelector('#modal-bg');
-const modalExit = document.querySelector('#modal-exit-button');
+//VARIABLES                                 
+const buttonSet = document.querySelector('#button-wrapper')
+const reviewBtn = document.querySelector('#review-button');
+const winnerHeader = document.querySelector('.winner-header');
+const reviewHistory = document.querySelector('.winner-board-wrapper');
+const playerX = document.querySelector('#playerX');
+const playerO = document.querySelector("#playerO");
+const playerWrapper = document.querySelector('#player-selection-wrapper');
+const boardWrapper = document.querySelector('#board-wrapper');
 const boardCells = document.querySelectorAll('.cell');
-const prevBtn = document.querySelector('.Previous');
-const nextBtn = document.querySelector('.Next')
-const resetBtn = document. querySelector('.Reset')
-const boardGame = document.querySelector('#actual-board')
-let player = true;
+const prevBtn = document.querySelector('.previous');
+const nextBtn = document.querySelector('.next');
+const resetBtn = document. querySelector('.reset');
+const boardGame = document.querySelector('#actual-board');
+let player = null;
 let gameHistory = 0;
 let drawCounter = 0;
+
+//TEST-AREA-FOR-VARIABLES-AND-FUNCTIONS
+
+//PLAYER SELECTION
+function playerSelection (mark)  {
+    if(mark === 'X') {
+        player = true;
+    } else {
+        player = false;
+    }
+    playerWrapper.classList.add('hide');
+    // playerWrapper.style.display = 'none';
+    boardWrapper.classList.add('show');
+}
 
 //WINNING CONDITIONS AND COMBINATIONS
 const winState = [
@@ -42,7 +62,6 @@ const selectCell = (e) => {
         cellData.innerHTML = 'X';
         playerMark = 'X';
     }
-
     classifyWinnner(playerMark);
     StoreMove(cellData);
 }
@@ -53,10 +72,8 @@ const StoreMove = () => {
     let row2 = []
     let row3 = []
     // console.log(cellIndex) //check in index is still passed
-
     boardCells.forEach((cell, cellIndex) => {
         // console.log(cell, cellIndex);
-
         if(cellIndex < 3){
             row1.push(cell.innerText);
         } else if (cellIndex >= 3 && cellIndex <=5) {
@@ -65,8 +82,6 @@ const StoreMove = () => {
             row3.push(cell.innerText);
         }
     })
-
-    
     gameState.push([row1, row2, row3]);
     gameHistory = gameState.length-1;
     // console.log(gameState); //Checking if gameState is storing
@@ -75,61 +90,47 @@ const StoreMove = () => {
 
 //Checks the GameState for possible winning combinations
 const classifyWinnner = (playerMark) => {
-
     for (let i = 0; i < boardCells.length; i++) {
         for (let j = 0; j < winState.length; j++) { 
             for (let k = 0; k < winState[j].length; k++) {
                 const tile1 = boardCells[winState[j][0]].innerText 
                 const tile2 = boardCells[winState[j][1]].innerText 
                 const tile3 = boardCells[winState[j][2]].innerText
-
                 if (tile1 === playerMark && tile2 === playerMark && tile3 === playerMark) {
-                    return console.log(`Player ${playerMark} Wins!`)
+                    reviewHistory.style.display = 'flex';
+                    winnerHeader.innerHTML = `Player ${playerMark} Wins!`;
+                    return;
                 } 
             }
         }
     }
-
     boardCells.forEach(cell => {
         if(cell.innerText != '') {
             drawCounter++
         }
     })
-
     console.log(drawCounter === 9);
-
     if(drawCounter === 9) {
         console.log(`Draw!`);
         //document
     }
-
     drawCounter = 0;
     // console.log(drawCounter);
-
-
 }
 
 function prevMove () {
-
-    
     // gameState[gameHistory];
     // console.log(gameHistory)
     // console.log(gameState[gameHistory])
-
     if(gameHistory > 0){
         gameHistory -= 1;
         loadTiles(gameHistory);
-        
     }
     console.log(gameState);
     console.log(gameHistory);
-
-    
 }
 
 function nextMove() {
-    
-
     if (gameHistory >= 0 && gameHistory < gameState.length-1){
         gameHistory += 1;
         loadTiles(gameHistory);
@@ -138,11 +139,9 @@ function nextMove() {
 
 function loadTiles (index) {
     boardGame.innerHTML = '';
-
     for (let i = 0; i < gameState[index].length; i++) {
         for (let j = 0; j < gameState[index][i].length; j++) {
             let div = document.createElement('div')
-
             div.innerText = gameState[index][i][j]
             div.classList.add('cell');
             // div.addEventListener('click', selectCell);
@@ -155,133 +154,44 @@ function loadTiles (index) {
 }
 
 function resetBoard() {
-    player = true;
-    gameHistory = 0;
-    drawCounter = 0;
+    location.reload();
+    // player = true;
+    // gameHistory = 0;
+    // drawCounter = 0;
 
 
-    gameState = []
-    boardGame.innerHTML = '';
+    // gameState = []
+    // boardGame.innerHTML = '';
 
-    for(let i = 0; i < 9; i++) {
-        let div = document.createElement('div')
+    // for(let i = 0; i < 9; i++) {
+    //     let div = document.createElement('div')
 
              
 
-            div.innerText = ''
-            div.classList.add('cell');
-            div.addEventListener('click', selectCell);
-            console.log(div)
-            boardGame.append(div)
-    }
+    //         div.innerText = ''
+    //         div.classList.add('cell');
+    //         div.addEventListener('click', selectCell);
+    //         console.log(div)
+    //         boardGame.append(div)
+    // }
+}
+
+function reviewFunction() {
+    reviewHistory.style.display = 'none';
+    buttonSet.style.display = 'flex';
 }
 
 
 
 //CLICK EVENTS
+playerX.addEventListener('click', function () {
+    playerSelection('X');
+});
+playerO.addEventListener('click',function () {
+    playerSelection('O');
+});
 boardCells.forEach(boardCell => boardCell.addEventListener('click', selectCell));
 prevBtn.addEventListener('click', prevMove);
 nextBtn.addEventListener('click', nextMove);
 resetBtn.addEventListener('click', resetBoard);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //VARIABLES
-// const boardCells = document.querySelectorAll('.cell');
-// let playerX = true;
-
-// //BOARD GAME STATE WHEN STARTING A PLAY
-
-
-// //Handle Click
-// const selectCell = (e) => {
-//     console.log(e.target);
-//     const cellInfo = e.target;
-//     const cellIndex = parseInt(cellInfo.getAttribute('data-section-index'));
-//     console.log(cellIndex);
-//     cellInfo.innerHTML = 'O';
-
-//     Prevent if game not running
-//     for ( const i = 0; i < gameState.length; i++) {
-//         const gameState = gameState[i]
-//         for ( const j = 0; j <gameState.length; j++){
-//             console.log(i + j);
-//         }
-//     }
-//     checkWin();
-// }
-
-
-// for ( let i = 0; i < gameState.length; i++) {
-//     let gameStates = gameState[i]
-//     for ( let j = 0; j <gameStates.length; j++){
-//         console.log(`[${i}][${j}] = ${gameStates[j]}`);
-//     }
-// }
-
-// //check winState
-// const checkWin = () => {
-//     for( let i = 0; i < boardCells.length; i++) {
-//         for ( let j = 0; j < winState.length; j++) {
-//             for ( let x = 0; x < winState[j].length; x++){
-//                 const cell = boardCells[winState[j][0]];
-//                 console.log(cell.getAttribute('data-section-index'));
-//             }
-//         }
-//     }
-// }
-
-
-// //Target
-// const cellClicked = (e) => {
-//     console.log(e.target);
-// }
-
-
-// boardCells.forEach(boardCell => boardCell.addEventListener('click', selectCell));
+reviewBtn.addEventListener('click', reviewFunction);
